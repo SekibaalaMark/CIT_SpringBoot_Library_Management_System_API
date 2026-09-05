751a8563a7e9f587277c799cf5fe589f05937254
sekibaalamark <markmajje256@gmail.com>
2026-09-05 23:55:27 +0300

    Added method to update full book and method to partially update a book

--- src/main/java/cit/backen/library/management/system/book/controller/BookController.java
+++ src/main/java/cit/backen/library/management/system/book/controller/BookController.java
@@ -2,6 +2,7 @@ package cit.backen.library.management.system.book.controller;
 
 
 import cit.backen.library.management.system.api.response.ApiResponse;
+import cit.backen.library.management.system.book.dto.BookPartialUpdateRequest;
 import cit.backen.library.management.system.book.dto.BookRequest;
 import cit.backen.library.management.system.book.dto.BookResponse;
 import cit.backen.library.management.system.book.service.BookService;
@@ -40,7 +41,18 @@ public class BookController {
     public ResponseEntity<ApiResponse<BookResponse>> getBookById(@PathVariable Long id){
         ApiResponse<BookResponse> apiResponse = bookService.getBookById(id);
         return ResponseEntity.status(HttpStatus.OK).body(apiResponse);
+    }
 
+    @PutMapping("/{id}")
+    public ResponseEntity<ApiResponse<BookResponse>> updateBookFully(@PathVariable Long id, @Valid @RequestBody BookRequest bookRequest){
+        ApiResponse<BookResponse> apiResponse = bookService.updateBookFully(id,bookRequest);
+        return ResponseEntity.status(HttpStatus.OK).body(apiResponse);
+    }
+
+    @PatchMapping("/{id}")
+    public ResponseEntity<ApiResponse<BookResponse>> updateBookPartial(@PathVariable Long id, @Valid @RequestBody BookPartialUpdateRequest request){
+        ApiResponse<BookResponse> apiResponse = bookService.updateBookPartial(id,request);
+        return ResponseEntity.status(HttpStatus.OK).body(apiResponse);
     }
 
 
