9e18aeecd22bf63141c7e67d08f3a34dd72fb3bc
sekibaalamark <markmajje256@gmail.com>
2026-09-05 16:24:46 +0300

    Create Book Edition Enums

---
+++ src/main/java/cit/backen/library/management/system/book/enums/Edition.java
@@ -0,0 +1,14 @@
+package cit.backen.library.management.system.book.enums;
+
+public enum Edition {
+    FIRST,
+    SECOND,
+    THIRD,
+    FOURTH,
+    FIFTH,
+    SIXTH,
+    SEVENTH,
+    EIGHTIETH,
+    NINTH,
+    TENTH
+}
