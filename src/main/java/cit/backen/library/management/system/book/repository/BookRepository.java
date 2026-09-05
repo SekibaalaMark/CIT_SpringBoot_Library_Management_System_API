a299633f7d536177b07eaa28891ceafde4a69cca
sekibaalamark <markmajje256@gmail.com>
2026-09-06 00:16:49 +0300

    added method to get book by isbn

--- src/main/java/cit/backen/library/management/system/book/repository/BookRepository.java
+++ src/main/java/cit/backen/library/management/system/book/repository/BookRepository.java
@@ -1,9 +1,14 @@
 package cit.backen.library.management.system.book.repository;
 
+import cit.backen.library.management.system.api.response.ApiResponse;
+import cit.backen.library.management.system.book.dto.BookResponse;
 import cit.backen.library.management.system.book.model.Book;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface BookRepository extends JpaRepository<Book,Long> {
+    Optional<Book> getBookByIsbn(String isbn);
 }
