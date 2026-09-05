efc7c4a24aaa64a2c434b30a53b12922b1c7a47e
sekibaalamark <markmajje256@gmail.com>
2026-09-06 02:06:19 +0300

    added method to add member

--- src/main/java/cit/backen/library/management/system/member/service/MemberService.java
+++ src/main/java/cit/backen/library/management/system/member/service/MemberService.java
@@ -1,10 +1,26 @@
 package cit.backen.library.management.system.member.service;
 
 
+import cit.backen.library.management.system.api.response.ApiResponse;
+import cit.backen.library.management.system.member.dto.MemberRequest;
+import cit.backen.library.management.system.member.dto.MemberResponse;
+import cit.backen.library.management.system.member.facade.MemberFacade;
+import cit.backen.library.management.system.member.repository.MemberRepository;
 import org.springframework.stereotype.Service;
 
 
 
 @Service
 public class MemberService {
+    private final MemberRepository memberRepository;
+    private final MemberFacade memberFacade;
+
+    public MemberService(MemberRepository memberRepository, MemberFacade memberFacade) {
+        this.memberRepository = memberRepository;
+        this.memberFacade = memberFacade;
+    }
+
+    public ApiResponse<MemberResponse> addMember(MemberRequest request){
+        return memberFacade.addMember(request);
+    }
 }
