a39850794ae9de7199beda27931c78073e448158
sekibaalamark <markmajje256@gmail.com>
2026-09-06 02:05:49 +0300

    added method to add member

--- src/main/java/cit/backen/library/management/system/member/controller/MemberController.java
+++ src/main/java/cit/backen/library/management/system/member/controller/MemberController.java
@@ -1,11 +1,30 @@
 package cit.backen.library.management.system.member.controller;
 
 
+import cit.backen.library.management.system.api.response.ApiResponse;
+import cit.backen.library.management.system.member.dto.MemberRequest;
+import cit.backen.library.management.system.member.dto.MemberResponse;
+import cit.backen.library.management.system.member.service.MemberService;
+import jakarta.validation.Valid;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/api/v1/members")
 public class MemberController {
+    private final MemberService memberService;
 
+    public MemberController(MemberService memberService) {
+        this.memberService = memberService;
+    }
+
+    @PostMapping
+    public ResponseEntity<ApiResponse<MemberResponse>> addMember(@Valid @RequestBody MemberRequest request){
+        ApiResponse<MemberResponse> apiResponse = memberService.addMember(request);
+        return ResponseEntity.status(HttpStatus.CREATED).body(apiResponse);
+    }
 }
