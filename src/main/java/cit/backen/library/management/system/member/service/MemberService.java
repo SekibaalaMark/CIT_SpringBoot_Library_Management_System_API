491bce1ab9127612fed54d22edc66889b2f0054e
sekibaalamark <markmajje256@gmail.com>
2026-09-06 09:07:55 +0300

    added method to get  member by id

--- src/main/java/cit/backen/library/management/system/member/service/MemberService.java
+++ src/main/java/cit/backen/library/management/system/member/service/MemberService.java
@@ -2,13 +2,18 @@ package cit.backen.library.management.system.member.service;
 
 
 import cit.backen.library.management.system.api.response.ApiResponse;
+import cit.backen.library.management.system.exceptions.member.MemberNotFoundException;
 import cit.backen.library.management.system.member.dto.MemberRequest;
 import cit.backen.library.management.system.member.dto.MemberResponse;
 import cit.backen.library.management.system.member.facade.MemberFacade;
+import cit.backen.library.management.system.member.mapper.MemberMapper;
+import cit.backen.library.management.system.member.model.Member;
 import cit.backen.library.management.system.member.repository.MemberRepository;
 import cit.backen.library.management.system.page.response.PageResponse;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestParam;
 
 
@@ -16,10 +21,12 @@ import org.springframework.web.bind.annotation.RequestParam;
 public class MemberService {
     private final MemberRepository memberRepository;
     private final MemberFacade memberFacade;
+    private final MemberMapper memberMapper;
 
-    public MemberService(MemberRepository memberRepository, MemberFacade memberFacade) {
+    public MemberService(MemberRepository memberRepository, MemberFacade memberFacade, MemberMapper memberMapper) {
         this.memberRepository = memberRepository;
         this.memberFacade = memberFacade;
+        this.memberMapper = memberMapper;
     }
 
     public ApiResponse<MemberResponse> addMember(MemberRequest request){
@@ -28,5 +35,13 @@ public class MemberService {
 
     public ApiResponse<PageResponse<MemberResponse>> getAllMembers(int page,int pageSize){
         return memberFacade.getAllMembers(page,pageSize);
+    }
+
+
+    public ApiResponse<MemberResponse> getMemberById(@PathVariable Long id){
+        Member member = memberRepository.findById(id)
+                .orElseThrow(()-> new MemberNotFoundException("id: "+id));
+        MemberResponse memberResponse = memberMapper.memberModelToResponse(member);
+        return new ApiResponse<>("SUCCESS","Member returned successfully",memberResponse);
     }
 }
