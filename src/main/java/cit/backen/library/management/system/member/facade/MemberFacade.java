f5024c543ea6906109efaa927b94e724c3f8091e
sekibaalamark <markmajje256@gmail.com>
2026-09-06 09:43:07 +0300

    added condition to check whether username already exists before adding new member

--- src/main/java/cit/backen/library/management/system/member/facade/MemberFacade.java
+++ src/main/java/cit/backen/library/management/system/member/facade/MemberFacade.java
@@ -2,6 +2,7 @@ package cit.backen.library.management.system.member.facade;
 
 
 import cit.backen.library.management.system.api.response.ApiResponse;
+import cit.backen.library.management.system.exceptions.member.MemberWithUsernameAlreadyExistsException;
 import cit.backen.library.management.system.member.dto.MemberRequest;
 import cit.backen.library.management.system.member.dto.MemberResponse;
 import cit.backen.library.management.system.member.mapper.MemberMapper;
@@ -30,8 +31,14 @@ public class MemberFacade {
 
     public ApiResponse<MemberResponse> addMember(MemberRequest request){
         Member member = memberMapper.memberRequestToModel(request);
+
+        if(memberRepository.existsByUsername(request.getUsername())){
+            throw new MemberWithUsernameAlreadyExistsException(request.getUsername());
+        }
+
         MemberResponse memberResponse = memberMapper.memberModelToResponse(memberRepository.save(member));
         return new ApiResponse<MemberResponse>("SUCCESS","Member added successfully",memberResponse);
+
     }
 
 
