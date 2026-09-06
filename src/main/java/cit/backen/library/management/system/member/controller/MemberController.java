00ffb31cf012ef1e3bc0a705927283ee6e51938e
sekibaalamark <markmajje256@gmail.com>
2026-09-06 08:56:37 +0300

    added method to get all members with pagination and sorting

--- src/main/java/cit/backen/library/management/system/member/controller/MemberController.java
+++ src/main/java/cit/backen/library/management/system/member/controller/MemberController.java
@@ -5,13 +5,11 @@ import cit.backen.library.management.system.api.response.ApiResponse;
 import cit.backen.library.management.system.member.dto.MemberRequest;
 import cit.backen.library.management.system.member.dto.MemberResponse;
 import cit.backen.library.management.system.member.service.MemberService;
+import cit.backen.library.management.system.page.response.PageResponse;
 import jakarta.validation.Valid;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/v1/members")
@@ -26,5 +24,14 @@ public class MemberController {
     public ResponseEntity<ApiResponse<MemberResponse>> addMember(@Valid @RequestBody MemberRequest request){
         ApiResponse<MemberResponse> apiResponse = memberService.addMember(request);
         return ResponseEntity.status(HttpStatus.CREATED).body(apiResponse);
+    }
+
+    @GetMapping
+    public ResponseEntity<ApiResponse<PageResponse<MemberResponse>>> getAllMembers(
+            @RequestParam(defaultValue = "1") int page,
+            @RequestParam(defaultValue = "10") int pageSize
+    ){
+        ApiResponse<PageResponse<MemberResponse>> apiResponse = memberService.getAllMembers(page,pageSize);
+        return ResponseEntity.status(HttpStatus.OK).body(apiResponse);
     }
 }
