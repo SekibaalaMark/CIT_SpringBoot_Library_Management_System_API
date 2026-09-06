05a1b4abff5f6669e28a2c7d3355f9b48a164a10
sekibaalamark <markmajje256@gmail.com>
2026-09-06 08:56:49 +0300

    added method to get all members with pagination and sorting

--- src/main/java/cit/backen/library/management/system/member/facade/MemberFacade.java
+++ src/main/java/cit/backen/library/management/system/member/facade/MemberFacade.java
@@ -7,7 +7,15 @@ import cit.backen.library.management.system.member.dto.MemberResponse;
 import cit.backen.library.management.system.member.mapper.MemberMapper;
 import cit.backen.library.management.system.member.model.Member;
 import cit.backen.library.management.system.member.repository.MemberRepository;
+import cit.backen.library.management.system.page.response.PageResponse;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Component;
+
+import java.util.List;
 
 
 @Component
@@ -24,5 +32,27 @@ public class MemberFacade {
         Member member = memberMapper.memberRequestToModel(request);
         MemberResponse memberResponse = memberMapper.memberModelToResponse(memberRepository.save(member));
         return new ApiResponse<MemberResponse>("SUCCESS","Member added successfully",memberResponse);
+    }
+
+
+    public ApiResponse<PageResponse<MemberResponse>> getAllMembers(int page, int pageSize){
+        int zeroBasedPage = Math.max(0,page-1);
+
+        Pageable pageable = PageRequest.of(zeroBasedPage,pageSize, Sort.by("username").ascending());
+        Page<Member> membersPage = memberRepository.findAll(pageable);
+        List<MemberResponse> memberResponseList = membersPage.getContent()
+                .stream()
+                .map(memberMapper::memberModelToResponse)
+                .toList();
+        PageResponse<MemberResponse> pageResponse = new PageResponse<>(
+                memberResponseList,
+                page,
+                pageSize,
+                membersPage.getTotalElements(),
+                membersPage.getTotalPages(),
+                membersPage.isLast()
+        );
+
+        return new ApiResponse<>("SUCCESS","Page of members",pageResponse);
     }
 }
