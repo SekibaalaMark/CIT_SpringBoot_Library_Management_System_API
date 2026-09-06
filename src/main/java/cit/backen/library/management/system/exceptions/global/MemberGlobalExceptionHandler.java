eb69d88c5768090a285ee427530d5b11f31f00b5
sekibaalamark <markmajje256@gmail.com>
2026-09-06 09:43:45 +0300

    added method to handle username already exists exception

--- src/main/java/cit/backen/library/management/system/exceptions/global/MemberGlobalExceptionHandler.java
+++ src/main/java/cit/backen/library/management/system/exceptions/global/MemberGlobalExceptionHandler.java
@@ -3,6 +3,7 @@ package cit.backen.library.management.system.exceptions.global;
 import cit.backen.library.management.system.api.response.ApiResponse;
 import cit.backen.library.management.system.exceptions.book.BookNotFoundException;
 import cit.backen.library.management.system.exceptions.member.MemberNotFoundException;
+import cit.backen.library.management.system.exceptions.member.MemberWithUsernameAlreadyExistsException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -13,10 +14,18 @@ import org.springframework.web.bind.annotation.RestControllerAdvice;
 @RestControllerAdvice
 public class MemberGlobalExceptionHandler {
 
-    @ExceptionHandler(BookNotFoundException.class)
-    public ResponseEntity<ApiResponse<Object>> MemberNotFoundException(MemberNotFoundException exception){
+    @ExceptionHandler(MemberNotFoundException.class)
+    public ResponseEntity<ApiResponse<Object>> handleMemberNotFoundException(MemberNotFoundException exception){
         ApiResponse<Object> apiResponse = new ApiResponse<>("NOT_FOUND", exception.getMessage(), null);
         return ResponseEntity.status(HttpStatus.NOT_FOUND).body(apiResponse);
     }
+
+    @ExceptionHandler(MemberWithUsernameAlreadyExistsException.class)
+    public ResponseEntity<ApiResponse<Object>> handleMemberWithUsernameAlreadyExistsException(MemberWithUsernameAlreadyExistsException exception){
+        ApiResponse<Object> apiResponse = new ApiResponse<>("BAD_REQUEST", exception.getMessage(), null);
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(apiResponse);
+    }
+
+
 }
 
