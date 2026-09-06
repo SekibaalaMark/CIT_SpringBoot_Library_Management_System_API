6ae90d557ed3c52b2be86dae05e5cf9ce3620856
sekibaalamark <markmajje256@gmail.com>
2026-09-06 12:44:46 +0300

    Added method to handle MemberWithNinNumberAlreadyExistException

--- src/main/java/cit/backen/library/management/system/exceptions/global/MemberGlobalExceptionHandler.java
+++ src/main/java/cit/backen/library/management/system/exceptions/global/MemberGlobalExceptionHandler.java
@@ -3,6 +3,7 @@ package cit.backen.library.management.system.exceptions.global;
 import cit.backen.library.management.system.api.response.ApiResponse;
 import cit.backen.library.management.system.exceptions.book.BookNotFoundException;
 import cit.backen.library.management.system.exceptions.member.MemberNotFoundException;
+import cit.backen.library.management.system.exceptions.member.MemberWithNinNumberAlreadyExistException;
 import cit.backen.library.management.system.exceptions.member.MemberWithUsernameAlreadyExistsException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -25,6 +26,15 @@ public class MemberGlobalExceptionHandler {
         ApiResponse<Object> apiResponse = new ApiResponse<>("BAD_REQUEST", exception.getMessage(), null);
         return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(apiResponse);
     }
+
+
+    @ExceptionHandler(MemberWithNinNumberAlreadyExistException.class)
+    public ResponseEntity<ApiResponse<Object>> handleMemberWithNinNumberAlreadyExistException(MemberWithNinNumberAlreadyExistException exception){
+        ApiResponse<Object> apiResponse = new ApiResponse<>("BAD_REQUEST", exception.getMessage(), null);
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(apiResponse);
+    }
+
+
 
 
 }
