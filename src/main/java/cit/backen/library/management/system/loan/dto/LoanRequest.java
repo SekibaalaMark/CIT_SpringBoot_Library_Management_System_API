1e2e67301ec76938399116ddf5d3cada24fc3d11
sekibaalamark <markmajje256@gmail.com>
2026-09-06 13:06:53 +0300

    Created Loan Request

---
+++ src/main/java/cit/backen/library/management/system/loan/dto/LoanRequest.java
@@ -0,0 +1,26 @@
+package cit.backen.library.management.system.loan.dto;
+
+import cit.backen.library.management.system.loan.enums.Status;
+import jakarta.validation.constraints.NotNull;
+import lombok.*;
+
+
+@Data
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class LoanRequest {
+
+    @NotNull(message = "Loan period cannot be null")
+    private Integer period;
+
+    @NotNull(message = "member id is required")
+    private Long memberId;
+
+    @NotNull(message = "book id is required")
+    private Long bookId;
+
+    @NotNull(message = "Loan Status is required")
+    private Status status;
+}
