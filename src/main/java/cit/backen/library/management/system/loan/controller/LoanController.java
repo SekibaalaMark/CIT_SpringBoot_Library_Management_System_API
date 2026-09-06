78479c752ccb4b0e20059763e35568b5d46e866d
sekibaalamark <markmajje256@gmail.com>
2026-09-06 13:58:49 +0300

    Added method to add a book loan

--- src/main/java/cit/backen/library/management/system/loan/controller/LoanController.java
+++ src/main/java/cit/backen/library/management/system/loan/controller/LoanController.java
@@ -1,4 +1,30 @@
 package cit.backen.library.management.system.loan.controller;
 
+
+import cit.backen.library.management.system.api.response.ApiResponse;
+import cit.backen.library.management.system.loan.dto.LoanRequest;
+import cit.backen.library.management.system.loan.dto.LoanResponse;
+import cit.backen.library.management.system.loan.service.LoanService;
+import jakarta.validation.Valid;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1/loans")
 public class LoanController {
+    private final LoanService loanService;
+
+    public LoanController(LoanService loanService) {
+        this.loanService = loanService;
+    }
+
+    @PostMapping
+    public ResponseEntity<ApiResponse<LoanResponse>> addLoan(@Valid @RequestBody LoanRequest request){
+        ApiResponse<LoanResponse> apiResponse = loanService.addLoan(request);
+        return ResponseEntity.status(HttpStatus.CREATED).body(apiResponse);
+    }
 }
