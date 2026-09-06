6a2c8b527993cffc293e7775f04448376226906e
sekibaalamark <markmajje256@gmail.com>
2026-09-06 09:44:32 +0300

    added methods to find member by username and one to check if  username already exists

--- src/main/java/cit/backen/library/management/system/member/repository/MemberRepository.java
+++ src/main/java/cit/backen/library/management/system/member/repository/MemberRepository.java
@@ -4,8 +4,13 @@ import cit.backen.library.management.system.member.model.Member;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 
 @Repository
 public interface MemberRepository extends JpaRepository<Member,Long> {
+    Optional<Member> findByUsername(String username);
+
+    Boolean existsByUsername(String username);
 
 }
