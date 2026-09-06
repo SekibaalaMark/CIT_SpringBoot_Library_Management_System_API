33b1d6ed0b274ab179a90a6e1029a958089b757e
sekibaalamark <markmajje256@gmail.com>
2026-09-06 13:59:59 +0300

    Added methods to map LoanRequest to Loan model and method to map loan model to LoanResponse

--- src/main/java/cit/backen/library/management/system/loan/facade/LoanFacade.java
+++ src/main/java/cit/backen/library/management/system/loan/facade/LoanFacade.java
@@ -1,4 +1,21 @@
 package cit.backen.library.management.system.loan.facade;
 
+
+import cit.backen.library.management.system.api.response.ApiResponse;
+import cit.backen.library.management.system.loan.dto.LoanRequest;
+import cit.backen.library.management.system.loan.dto.LoanResponse;
+import cit.backen.library.management.system.loan.mapper.LoanMapper;
+import cit.backen.library.management.system.loan.repository.LoanRepository;
+import org.springframework.stereotype.Component;
+
+@Component
 public class LoanFacade {
+    private final LoanMapper loanMapper;
+    private final LoanRepository loanRepository;
+
+    public LoanFacade(LoanMapper loanMapper, LoanRepository loanRepository) {
+        this.loanMapper = loanMapper;
+        this.loanRepository = loanRepository;
+    }
+
 }
