447ae06b1984a2d85960e335a7a7aebc06b99349
sekibaalamark <markmajje256@gmail.com>
2026-09-08 12:24:17 +0300

    Added query to fetch total active loans by member

--- src/main/java/cit/backen/library/management/system/loan/repository/LoanRepository.java
+++ src/main/java/cit/backen/library/management/system/loan/repository/LoanRepository.java
@@ -1,11 +1,17 @@
 package cit.backen.library.management.system.loan.repository;
 
 
+import cit.backen.library.management.system.loan.enums.Status;
 import cit.backen.library.management.system.loan.model.Loan;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface LoanRepository extends JpaRepository<Loan,Long> {
+
+    @Query("SELECT COUNT(l) FROM Loan l WHERE l.member.id = :memberId AND l.status = :status")
+    long countActiveLoansByMemberId(@Param("memberId") Long memberId, @Param("status") Status status);
 
 }
