0aba9f2e85a8008746182850848df33a43a6fddf
sekibaalamark <markmajje256@gmail.com>
2026-09-08 13:21:44 +0300

    Added method to handle BookUnavailableException

--- src/main/java/cit/backen/library/management/system/exceptions/global/BookGlobalExceptionHandler.java
+++ src/main/java/cit/backen/library/management/system/exceptions/global/BookGlobalExceptionHandler.java
@@ -2,6 +2,7 @@ package cit.backen.library.management.system.exceptions.global;
 
 import cit.backen.library.management.system.api.response.ApiResponse;
 import cit.backen.library.management.system.exceptions.book.BookNotFoundException;
+import cit.backen.library.management.system.exceptions.book.BookUnavailableException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -14,6 +15,12 @@ public class BookGlobalExceptionHandler {
     @ExceptionHandler(BookNotFoundException.class)
     public ResponseEntity<ApiResponse<Object>> handleBookNotFoundException(BookNotFoundException exception){
         ApiResponse<Object> apiResponse = new ApiResponse<>("NOT_FOUND", exception.getMessage(), null);
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(apiResponse);
+    }
+
+    @ExceptionHandler(BookUnavailableException.class)
+    public ResponseEntity<ApiResponse<Object>> handleBookUnavailableException(BookUnavailableException exception){
+        ApiResponse<Object> apiResponse = new ApiResponse<>("BAD_REQUEST", exception.getMessage(), null);
         return ResponseEntity.status(HttpStatus.NOT_FOUND).body(apiResponse);
     }
 }
